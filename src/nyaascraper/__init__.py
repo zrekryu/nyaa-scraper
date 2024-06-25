0c95ab323e180c980cabb48c54144d73a095cf27
Zrekryu <145272593+zrekryu@users.noreply.github.com>
2024-06-26 03:57:06 +0530

    Add files via upload

--- src/nyaascraper/__init__.py
+++ src/nyaascraper/__init__.py
@@ -1,10 +1,3 @@
 from ._version import __version__
 
 from .client import NyaaClient
-from .enums import (
-    SITE,
-    Filter,
-    FunCategory, FapCategory,
-    SortBy, SortOrder,
-    TorrentType
-    )
